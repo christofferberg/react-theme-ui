2ce66ca9f1b8d55e4324d673e2f84fbc0ffefc7e
Amir Khan <aokhan192@gmail.com>
2020-01-29 22:51:50 +0100

    Button component with events

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const styles = {
   bg: 'primary',
 }
 
+// @ts-ignore
 const App: FC = () => (
   <ThemeProvider theme={theme}>
     <Header />
@@ -21,9 +22,12 @@ const App: FC = () => (
     <Container p={4} bg={'muted'}>
       <Heading variant={'large'}>Heading h1</Heading>
       <Heading variant={'small'}>Heading h1</Heading>
-
-      <Button variant={'primary'}>Button Primary</Button>
-      <Button variant={'secondary'}>Button Secondary</Button>
+      <Button variant={'primary'} onClick={(e) => console.log(e.target)}>
+        Button Primary
+      </Button>
+      <Button variant={'secondary'} onMouseDown={(e) => console.log('Hello')}>
+        Button Secondary
+      </Button>
 
       <Styled.h1>Hello, styled heading!</Styled.h1>
     </Container>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,29 +1,30 @@
 /** @jsx jsx */
-import React, { FC } from 'react'
+import React, { ComponentPropsWithRef } from 'react'
 import { jsx } from 'theme-ui'
+
+export interface ButtonProps extends ComponentPropsWithRef<'button'>, Props {}
 
 type Props = {
   variant: string
 }
 
-const Button: FC<Props> = ({ variant = 'primary', ...props }) => {
-  return (
-    <button
-      {...props}
-      sx={{
-        appearance: 'none',
-        display: 'inline-block',
-        textAlign: 'center',
-        lineHeight: 'inherit',
-        textDecoration: 'none',
-        fontSize: 'inherit',
-        border: 0,
-        borderRadius: 4,
-        // pass variant prop to sx
-        variant: ['buttons.default', `buttons.${variant}`],
-      }}
-    />
-  )
-}
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => (
+  <button
+    {...props}
+    ref={ref}
+    sx={{
+      appearance: 'none',
+      display: 'inline-block',
+      textAlign: 'center',
+      lineHeight: 'inherit',
+      textDecoration: 'none',
+      fontSize: 'inherit',
+      border: 0,
+      borderRadius: 4,
+      // pass variant prop to sx
+      variant: ['buttons.default', `buttons.${props.variant}`],
+    }}
+  />
+))
 
 export default Button
